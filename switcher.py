31eeb4cd763d232b50f156761ced10d7b6c9f7c0
FOxDoryu <guzz9777@gmail.com>
2025-07-24 21:49:57 +0700

    Delete switcher.py

--- switcher.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-if "page" not in st.session_state:
-    st.session_state.page = "home"
-
-if st.session_state.page == "home":
-    import home
-elif st.session_state.page == "page2":
-    import page2
